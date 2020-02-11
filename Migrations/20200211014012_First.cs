3274b8f420ff372534c2e0626814bd07e81d208b
Georgewisdom <coppycatter100@gmail.com>
2020-02-11 03:20:07 +0100

    initial commit

---
+++ Migrations/20200211014012_First.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    EmpCode = table.Column<string>(type: "varchar(10)", nullable: false),
+                    Position = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Location = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName ="nvarchar(250)")]
+        public string FullName { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        public int EmpCode { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Location { get; set; }
+    }
+}
